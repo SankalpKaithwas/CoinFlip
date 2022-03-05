2745dad6872fb7b45c8f0bd0b655d3c20894806a
unknown <kaithwas.697@gmail.com>
2022-03-05 11:36:59 +0530

    First commit

---
+++ Coin Flip/Coin Flip/Program.cs
@@ -0,0 +1,10 @@
+﻿
+Random random = new Random();
+int coinCheck = random.Next(0, 2);
+if (coinCheck == 1)
+{
+    Console.WriteLine("You got Head");
+}else
+{
+    Console.WriteLine("You got Tail");
+}
